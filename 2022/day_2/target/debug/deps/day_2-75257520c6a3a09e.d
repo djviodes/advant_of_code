02c743009b7bab3c9c668fc0e487444fbd52b538
David Viodes <david.viodes@compassmining.io>
2023-07-28 00:33:33 -0700

    Organizing

---
+++ 2022/day_2/target/debug/deps/day_2-75257520c6a3a09e.d
@@ -0,0 +1,5 @@
+/Users/davidviodes/Practice/Rust/advant_of_code/day_2/target/debug/deps/day_2-75257520c6a3a09e.rmeta: src/main.rs
+
+/Users/davidviodes/Practice/Rust/advant_of_code/day_2/target/debug/deps/day_2-75257520c6a3a09e.d: src/main.rs
+
+src/main.rs:
---
+++ 2022/day_2/target/debug/deps/day_2-f3418eda77b3348c.d
@@ -0,0 +1,5 @@
+/Users/davidviodes/Practice/Rust/advant_of_code/day_2/target/debug/deps/day_2-f3418eda77b3348c.rmeta: src/main.rs
+
+/Users/davidviodes/Practice/Rust/advant_of_code/day_2/target/debug/deps/day_2-f3418eda77b3348c.d: src/main.rs
+
+src/main.rs:
