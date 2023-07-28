fb48b9813fa5aa5ea60fb9873c69cd3f4de81bad
David Viodes <david.viodes@compassmining.io>
2023-07-28 16:15:59 -0700

    Set up day 3 template

--- 2022/day_3/src/main.rs
+++ 2022/day_3/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Calculating the total priority for the item that appears in both comparments of each rucksack!");
+
+    calculate_priority();
+}
+
+fn calculate_priority() {
+    
 }
