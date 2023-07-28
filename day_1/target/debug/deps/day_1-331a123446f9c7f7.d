7703082bf347ab4accc75448d00734980a400c65
David Viodes <david.viodes@compassmining.io>
2023-07-27 23:31:51 -0700

    Initialized a new rust project for day 1

---
+++ day_1/target/debug/deps/day_1-331a123446f9c7f7.d
@@ -0,0 +1,5 @@
+/Users/davidviodes/Practice/Rust/advant_of_code/day_1/target/debug/deps/day_1-331a123446f9c7f7.rmeta: src/main.rs
+
+/Users/davidviodes/Practice/Rust/advant_of_code/day_1/target/debug/deps/day_1-331a123446f9c7f7.d: src/main.rs
+
+src/main.rs:
---
+++ day_1/target/debug/deps/day_1-49d27291ce98aafa.d
@@ -0,0 +1,5 @@
+/Users/davidviodes/Practice/Rust/advant_of_code/day_1/target/debug/deps/day_1-49d27291ce98aafa.rmeta: src/main.rs
+
+/Users/davidviodes/Practice/Rust/advant_of_code/day_1/target/debug/deps/day_1-49d27291ce98aafa.d: src/main.rs
+
+src/main.rs:
